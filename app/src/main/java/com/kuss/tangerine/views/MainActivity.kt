97350c58904cc6e72796fc51e02945191a16ceee
Kuss <2360314753@qq.com>
2019-11-04 23:34:13 +0800

    :lipstick: update: add BottomAppBar to replaced AppBarLayout

--- app/src/main/java/com/kuss/tangerine/views/MainActivity.kt
+++ app/src/main/java/com/kuss/tangerine/views/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(bar)
 
         taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
 
