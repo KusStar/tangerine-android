a35b3e52faf9f5fe11f9c7d53958f71381026ed0
Kuss <2360314753@qq.com>
2019-11-05 23:04:31 +0800

    :construction: progressing: add TaskType.kt, TaskIconMapping.kt and remove DefaultText.kt

---
+++ app/src/main/java/com/kuss/tangerine/util/constants/TaskIconMapping.kt
@@ -0,0 +1,48 @@
+package com.kuss.tangerine.util.constants
+
+import com.kuss.tangerine.R
+
+class TaskIconMapping {
+    val ids = arrayOf(
+        R.mipmap.art,
+        R.mipmap.bookmark,
+        R.mipmap.bug,
+        R.mipmap.dizzy,
+        R.mipmap.feat,
+        R.mipmap.fire,
+        R.mipmap.lipstick,
+        R.mipmap.memo,
+        R.mipmap.pencil,
+        R.mipmap.poop,
+        R.mipmap.refactor,
+        R.mipmap.tada
+    )
+    val names = arrayOf(
+        "art",
+        "bookmark",
+        "bug",
+        "dizzy",
+        "feat",
+        "fire",
+        "lipstick",
+        "memo",
+        "pencil",
+        "poop",
+        "refactor",
+        "tada"
+    )
+    val alias = arrayOf(
+        "improve",
+        "mark",
+        "fix",
+        "animation",
+        "feat",
+        "remove",
+        "update",
+        "typo",
+        "write",
+        "shit",
+        "refactor",
+        "init"
+    )
+}
